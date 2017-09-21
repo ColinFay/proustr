780bfff0a3a76e215d9e920159e7a034596fcea7
Colin <colin@thinkr.fr>
2017-09-21 22:34:08 +0200

    clean data

--- data-raw/prep_data.R
+++ data-raw/prep_data.R
@@ -42,11 +42,13 @@ ducotedechezswann2 <- get_book_wikisource("https://fr.wikisource.org/wiki/Du_c%C
 ducotedechezswann2$year <- 1913
 ducotedechezswann3 <- get_book_wikisource("https://fr.wikisource.org/wiki/Du_c%C3%B4t%C3%A9_de_chez_Swann/Partie_3")
 ducotedechezswann <- rbind(ducotedechezswann1, ducotedechezswann2, ducotedechezswann3)
+ducotedechezswann <- pr_normalize_punc(ducotedechezswann,text)
 
 alombredesjeunesfillesenfleurs1 <- get_book_wikisource("https://fr.wikisource.org/wiki/%C3%80_l%E2%80%99ombre_des_jeunes_filles_en_fleurs/Premi%C3%A8re_partie")
 alombredesjeunesfillesenfleurs2 <- get_book_wikisource("https://fr.wikisource.org/wiki/%C3%80_l%E2%80%99ombre_des_jeunes_filles_en_fleurs/Deuxi%C3%A8me_partie")
 alombredesjeunesfillesenfleurs3 <- get_book_wikisource("https://fr.wikisource.org/wiki/%C3%80_l%E2%80%99ombre_des_jeunes_filles_en_fleurs/Troisi%C3%A8me_partie")
 alombredesjeunesfillesenfleurs <- rbind(alombredesjeunesfillesenfleurs1, alombredesjeunesfillesenfleurs2, alombredesjeunesfillesenfleurs3)
+alombredesjeunesfillesenfleurs <- pr_normalize_punc(alombredesjeunesfillesenfleurs,text)
 
 lecotedeguermantes1 <- get_book_wikisource("https://fr.wikisource.org/wiki/Le_C%C3%B4t%C3%A9_de_Guermantes/Premi%C3%A8re_partie")
 lecotedeguermantes1$year <- 1921
@@ -55,6 +57,7 @@ lecotedeguermantes2$year <- 1921
 lecotedeguermantes3 <- get_book_wikisource("https://fr.wikisource.org/wiki/Le_C%C3%B4t%C3%A9_de_Guermantes/Troisi%C3%A8me_partie")
 lecotedeguermantes3$year <- 1921
 lecotedeguermantes <- rbind(lecotedeguermantes1, lecotedeguermantes2, lecotedeguermantes3)
+lecotedeguermantes <- pr_normalize_punc(lecotedeguermantes,text)
 
 sodomeetgomorrhe1 <- get_book_wikisource("https://fr.wikisource.org/wiki/Sodome_et_Gomorrhe/Partie_1")
 sodomeetgomorrhe1$volume <- "Première Partie"
@@ -66,23 +69,29 @@ sodomeetgomorrhe2 <- rbind(get_book_wikisource("https://fr.wikisource.org/wiki/S
 sodomeetgomorrhe2$volume <- "Deuxième Partie"
 sodomeetgomorrhe2$year <- 1922
 sodomeetgomorrhe <- rbind(sodomeetgomorrhe1, sodomeetgomorrhe2)
+sodomeetgomorrhe <- pr_normalize_punc(sodomeetgomorrhe,text)
 
 laprisonniere <- rbind(get_book_wikisource("https://fr.wikisource.org/wiki/La_Prisonni%C3%A8re/Chapitre_1"), 
                        get_book_wikisource("https://fr.wikisource.org/wiki/La_Prisonni%C3%A8re/Chapitre_2"),
                        get_book_wikisource("https://fr.wikisource.org/wiki/La_Prisonni%C3%A8re/Chapitre_3"))
 laprisonniere$year <- 1923
 laprisonniere$volume <- "Tome Unique"
+laprisonniere <- pr_normalize_punc(laprisonniere,text)
+
 
 albertinedisparue <- rbind(get_book_wikisource("https://fr.wikisource.org/wiki/Albertine_disparue/Chapitre_I"), 
                            get_book_wikisource("https://fr.wikisource.org/wiki/Albertine_disparue/Chapitre_II"), 
                            get_book_wikisource("https://fr.wikisource.org/wiki/Albertine_disparue/Chapitre_III"),
                            get_book_wikisource("https://fr.wikisource.org/wiki/Albertine_disparue/Chapitre_IV"))
 albertinedisparue$volume <- "Tome Unique"
+albertinedisparue <- pr_normalize_punc(albertinedisparue,text)
 
 letempretrouve <- rbind(get_book_wikisource("https://fr.wikisource.org/wiki/Le_Temps_retrouv%C3%A9/I"), 
                         get_book_wikisource("https://fr.wikisource.org/wiki/Le_Temps_retrouv%C3%A9/II"),
                         get_book_wikisource("https://fr.wikisource.org/wiki/Le_Temps_retrouv%C3%A9/III"))
 letempretrouve$volume <- "Tome Unique"
+letempretrouve <- pr_normalize_punc(letempretrouve,text)
+
 
 # Get characters
 url <- read_html("http://proust-personnages.fr/")
@@ -115,7 +124,7 @@ stopwords4 <- read_html("http://www.naunaute.com/liste-stop-words-francais-393")
 stopwords5 <- readLines("https://sites.google.com/site/kevinbouge/stopwords-lists/stopwords_fr.txt?attredirects=0&d=1") %>%
   as.tibble()
 
-stop_words <- c(stopwords$value, stopwords2$c..alors....au....aucuns....aussi....autre....avant....avec..., stopwords3$a, stopwords4$c..à....à.demi....à.peine....à.peu.près....absolument..., stopwords5$value) %>%
+stop_words_fr <- c(stopwords$value, stopwords2$c..alors....au....aucuns....aussi....autre....avant....avec..., stopwords4$c..à....à.demi....à.peine....à.peu.près....absolument..., stopwords5$value) %>%
   unique()
 
 # Sentiments 
@@ -139,6 +148,6 @@ devtools::use_data(laprisonniere, overwrite = TRUE)
 devtools::use_data(albertinedisparue, overwrite = TRUE)
 devtools::use_data(letempretrouve, overwrite = TRUE)
 devtools::use_data(proust_char, overwrite = TRUE)
-devtools::use_data(stop_words, overwrite = TRUE)
+devtools::use_data(stop_words_fr, overwrite = TRUE)
 devtools::use_data(sentiments_polarity, overwrite = TRUE)
 devtools::use_data(sentiments_score, overwrite = TRUE)
